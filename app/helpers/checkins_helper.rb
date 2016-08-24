fb37995a30112db3778e30987b65cb1d9c71168f
Zachary Cohn <zaccohn@gmail.com>
2016-08-24 18:09:51 -0400

    added checkinsHelper

--- app/helpers/checkins_helper.rb
+++ app/helpers/checkins_helper.rb
@@ -3,13 +3,18 @@ module CheckinsHelper
 	def setLabel color
 		if color == "green"
 			labelType = "label label-success"
+		elsif color == "yellow"
+			labelType = "label label-warning"
+		elsif color == "red"
+			labelType = "label label-danger"
 		end
 
-		"<div class=\"" + labelType + "\">"
+		"<div class=\"#{labelType}\">".html_safe
 	end
 
-	def buildButton color
-		"<%= link_to \"" + color.capitalize + "\", habit_checkin_path(status: \"" + color + "\", habit_id: h.id, id: last_id, date: checkinDate), method: \'PATCH\' %>"
+	def buildPatchButton color, id, last_id, checkinDate
+
+		"link_to \"#{color.capitalize}\", habit_checkin_path(status: \"#{color}\", habit_id: #{id}, id: #{last_id}, date: #{checkinDate}), method: 'PATCH'"
 	end
 
 end
