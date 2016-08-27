15cbc11d1354f91df829315dc84307ed395d510b
Zachary Cohn <zaccohn@gmail.com>
2016-08-26 23:48:01 -0400

    refactored helper, today, and week views. added button nav

--- app/helpers/checkins_helper.rb
+++ app/helpers/checkins_helper.rb
@@ -1,20 +1,36 @@
 module CheckinsHelper
 
-	def setLabel color
-		if color == "green"
-			labelType = "label label-success"
+	def setLabel(color)
+		labelType = if color == "green"
+			"label label-success"
 		elsif color == "yellow"
-			labelType = "label label-warning"
+			"label label-warning"
 		elsif color == "red"
-			labelType = "label label-danger"
+			"label label-danger"
 		end
 
 		"<div class=\"#{labelType}\">".html_safe
 	end
 
-	def buildPatchButton color, id, last_id, checkinDate
+	def buildCheckinLink(color, id, last_id, checkinDate, updateMethod)
 
-		"link_to \"#{color.capitalize}\", habit_checkin_path(status: \"#{color}\", habit_id: #{id}, id: #{last_id}, date: #{checkinDate}), method: 'PATCH'"
+		labelType = if color == "green"
+			"label label-success"
+		elsif color == "yellow"
+			"label label-warning"
+		elsif color == "red"
+			"label label-danger"
+		end
+
+		passBack = "<h3><div class=\"#{labelType}\">".html_safe
+		if last_id.nil?
+			passBack = passBack + link_to(color.capitalize, habit_checkins_path(status: color, habit_id: id, date: checkinDate), method: updateMethod) + "</span></h3>".html_safe
+
+		else
+			passBack = passBack + link_to(color.capitalize, habit_checkin_path(status: color, habit_id: id, id: last_id, date: checkinDate), method: updateMethod) + "</span></h3>".html_safe
+		end
+
+		passBack
 	end
 
 end
