43996633c44ab551d4bcecdf4caf13d2b4fbc7f5
Zachary Cohn <zaccohn@gmail.com>
2017-02-19 12:16:28 -0500

    passing tests

--- test/controllers/checkins_controller_test.rb
+++ test/controllers/checkins_controller_test.rb
@@ -6,12 +6,12 @@ class CheckinsControllerTest < ActionController::TestCase
   test "create a Checkin" do
   	sign_in(users(:ralph))
 
-  	checkinTime = Date.current
+  	checkinTime = setDate() 
   	assert_difference 'Checkin.count', 1 do
 	    post :create, habit_id: habits(:pants).id, date: checkinTime, status: "green"
 		end
 
-		assert_redirected_to "/habits"
+		assert_redirected_to "/habits/2017/2/18"
 
 		@checkin = Checkin.last
 		assert_equal "green", @checkin.status
@@ -25,22 +25,28 @@ class CheckinsControllerTest < ActionController::TestCase
 	test "update a Checkin" do
 		sign_in(users(:ralph))
 		
-		checkin_date = (Date.current - 5.days).strftime("%Y-%m-%d")
+#		checkin_date = (Date.current - 5.days).strftime("%Y-%m-%d")
+		checkin_date = setDate() - 5.days
 		post :create, habit_id: habits(:pants).id, date: checkin_date, status: "green"
-		assert_redirected_to "/habits"
+		assert_redirected_to "/habits/2017/2/13"
 
 		@checkin = Checkin.last
 		assert_equal "green", @checkin.status
 
 		patch :update, habit_id: habits(:pants).id, date: checkin_date, status: "red", id: habits(:pants).checkins.last.id
-		assert_redirected_to "/habits"
+		assert_redirected_to "/habits/2017/2/13"
 
 		@checkin.reload
 
-		assert_equal checkin_date, @checkin.date.to_formatted_s
+		assert_equal checkin_date.to_formatted_s(:db), @checkin.date.to_formatted_s
 		assert_equal "updated", assigns[:checkin].description  # to test variables from controller
 		assert_equal "red", @checkin.status
 		assert_equal "red", flash[:notice]
+	end
+
+	private
+	def setDate
+		currentDate = Date.new(2017,02,18) 
 	end
 end
 
